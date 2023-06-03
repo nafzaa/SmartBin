b65718588795945228ff11d1187431bab24016e4
Akif <akif.frees@gmail.com>
2023-06-03 23:28:10 +0800

    Delete Dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Dashboard
-Route::get('/dashboard', function(){
-    return view('pages.dashboard');
-});
