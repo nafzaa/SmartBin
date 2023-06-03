402b5670d8fb07ae9d03db5e37b74e1c7c08b359
Akif <akif.frees@gmail.com>
2023-06-04 01:58:56 +0800

    adjust dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,3 +1,13 @@
 <div>
-    <button class="btn btn-danger" wire:click='delete'>delete</button>
+    <div class="pagetitle">
+        <h1>Dashboard</h1>
+        <nav>
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item active"><a href="{{route('dashboard')}}">Dashboard</a></li>
+          </ol>
+        </nav>
+    </div><!-- End Page Title -->
+    <section class="section dashboard">
+        @include('components.Dashboard.Section.services')
+    </section>
 </div>
